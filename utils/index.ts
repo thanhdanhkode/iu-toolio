54d8d78afa59f60f54fa49d153e5a9a614a30ea1
thanhdanhkode <bfffamilk@gmail.com>
2025-11-30 12:22:44 +0700

    feat: add makeId function in utils

---
+++ utils/index.ts
@@ -0,0 +1,13 @@
+const makeId = (length: number) => {
+  let result = "",
+    characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+    charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+export { makeId };
