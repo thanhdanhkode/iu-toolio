058925dc79594a495bf49a38c6ec274ae1d0869d
thanhdanhkode <bfffamilk@gmail.com>
2025-11-30 12:20:46 +0700

    feat(ui): add Shadcn UI to project

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'wxt';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "wxt";
+import packageJson from "./package.json";
 
+// cSpell:ignore IU Toolio
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
+  vite: () => ({
+    plugins: [tailwindcss()],
+  }),
 });
