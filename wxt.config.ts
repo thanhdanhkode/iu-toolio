4802bd482bf1e31b0d60cc1d6a6e619000c29337
thanhdanhkode <bfffamilk@gmail.com>
2025-11-30 12:23:22 +0700

    build: custom manifest build with makeId

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "wxt";
 import packageJson from "./package.json";
+import { makeId } from "./utils/index";
 
 // cSpell:ignore IU Toolio
 // See https://wxt.dev/api/config.html
@@ -8,5 +9,17 @@ export default defineConfig({
   modules: ["@wxt-dev/module-react"],
   vite: () => ({
     plugins: [tailwindcss()],
+  }),
+  manifest: ({ browser, manifestVersion, mode, command }) => ({
+    name: `IU Toolio ${mode === "development" ? "(Dev)" : ""}`,
+    version: packageJson.version,
+    version_name:
+      packageJson.version +
+      `${mode === "development" ? " dev-" : " build-"}${makeId(8)}`,
+    description: "An app extension for IU students.",
+    action: {
+      default_title: "IU Toolio",
+    },
+    permissions: ["storage", "scripting", "tabs", "system.display"],
   }),
 });
