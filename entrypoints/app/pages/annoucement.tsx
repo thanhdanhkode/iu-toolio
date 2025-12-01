3eb9fc7e139fddbedb9172428bf40bb51595cc3c
thanhdanhkode <bfffamilk@gmail.com>
2025-12-01 08:39:05 +0700

    chore: update announcement page

--- entrypoints/app/pages/annoucement.tsx
+++ entrypoints/app/pages/annoucement.tsx
@@ -1,5 +1,12 @@
+import { Logo } from "@/components/icons/logo"
+
 const AnnouncementPage = () => {
-  return <div>AnnouncementPage</div>
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center">
+      <Logo className="size-32" />
+      <span className="mt-4 text-2xl font-medium">Welcome to IU Toolio</span>
+    </div>
+  )
 }
 
 export default AnnouncementPage
