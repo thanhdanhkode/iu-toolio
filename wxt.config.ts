12eb496041c2f0553e9c517297e3a03ddb3d372d
thanhdanhkode <bfffamilk@gmail.com>
2025-12-05 21:00:49 +0700

    chore: create en locale

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,24 +1,26 @@
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "wxt"
 import packageJson from "./package.json"
-import { makeId } from "./src/utils/index"
+import { makeId } from "./src/lib/make-id"
 
 // cSpell:ignore IU Toolio
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: "src",
-  modules: ["@wxt-dev/module-react"],
+  modules: ["@wxt-dev/module-react", "@wxt-dev/i18n/module"],
   vite: () => ({
     plugins: [tailwindcss()],
   }),
-  manifest: ({ browser, manifestVersion, mode, command }) => ({
+  manifest: ({ mode }) => ({
     name: `IU Toolio ${mode === "development" ? "(Dev)" : ""}`,
     version: packageJson.version,
     version_name: packageJson.version + `${mode === "development" ? " dev-" : " build-"}${makeId(8)}`,
     description: "An app extension for IU students.",
+    default_locale: "en",
     action: {
       default_title: "IU Toolio",
     },
-    permissions: ["storage", "scripting", "tabs", "system.display"],
+    permissions: ["storage", "scripting", "tabs", "system.display", "offscreen", "cookies"],
+    host_permissions: ["https://edusoftweb.hcmiu.edu.vn/*"],
   }),
 })
