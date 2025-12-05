3d596cabf010e605767e937b6fdc8e41cc9ac6d2
thanhdanhkode <bfffamilk@gmail.com>
2025-12-05 21:00:10 +0700

    feat: update event listener to background

--- src/entrypoints/background/index.ts
+++ src/entrypoints/background/index.ts
@@ -1,6 +1,20 @@
-import { PublicPath } from "wxt/browser"
-declare const self: ServiceWorkerGlobalScope
+import { onMessage } from "@/lib/messaging"
+import { EdusoftWebInstance } from "@/modules/edusoftweb"
+import { BackgroundEventListeners } from "./listeners"
 
 export default defineBackground(async () => {
   console.log("Hello background!", { id: browser.runtime.id })
+
+  browser.runtime.onInstalled.addListener(BackgroundEventListeners.onInstalled)
+
+  const browserAction = browser.action || browser.browserAction
+  browserAction.onClicked.addListener(BackgroundEventListeners.onActionClicked)
+
+  browser.runtime.onStartup.addListener(BackgroundEventListeners.onStartup)
+
+  EdusoftWebInstance.login("EEACIU24018", "790198")
+
+  onMessage("getCourseList", (message) => {
+    return EdusoftWebInstance.getCourse(message.data)
+  })
 })
