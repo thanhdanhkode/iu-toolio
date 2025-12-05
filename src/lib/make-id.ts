a939ab291f2ff3ea6ec91dcee068e79a8f73a210
thanhdanhkode <bfffamilk@gmail.com>
2025-12-05 20:57:50 +0700

    refactor: move file

--- src/lib/make-id.ts
+++ src/lib/make-id.ts
@@ -1,7 +1,7 @@
 const makeId = (length: number) => {
-  let result = "",
-    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
-    charactersLength = characters.length
+  let result = ""
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  const charactersLength = characters.length
 
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength))
