98bf9efae884fc57aa9bbef1c01dadd1573a9613
thanhdanhkode <bfffamilk@gmail.com>
2025-12-05 20:58:13 +0700

    chore: add base module

---
+++ src/modules/base/index.ts
@@ -0,0 +1,34 @@
+// cSpell:ignore axios-cookiejar-support
+
+import axios, { AxiosInstance } from "axios"
+import { wrapper } from "axios-cookiejar-support"
+import * as cheerio from "cheerio"
+import { CookieJar } from "tough-cookie"
+
+export class BaseInstance {
+  /**
+   * Create a pre-config Axios Instance
+   * @param baseURL host
+   * @returns AxiosInstance
+   */
+  protected static createClient(baseURL: string): AxiosInstance {
+    const jar = new CookieJar()
+    return wrapper(
+      axios.create({
+        baseURL,
+        jar,
+        withCredentials: true,
+        timeout: 10000,
+      })
+    )
+  }
+
+  /**
+   * Helper to load HTML string
+   * @param html HTML string to load
+   * @returns Cheerio
+   */
+  protected static htmlLoader(html: string) {
+    return cheerio.load(html)
+  }
+}
